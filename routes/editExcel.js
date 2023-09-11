017f68a916f13da57e62bd1394ccac3b60e857fa
higayoshiko <higayoshiko@yahoo.com>
2023-09-11 12:16:08 +0900

    compare and edit email,  export excel

--- routes/editExcel.js
+++ routes/editExcel.js
@@ -4,35 +4,6 @@ const https = require('node:https');
 const xlsx = require('xlsx');
 
 
-// router.post("/", function(req, res) {
-//   // let excelFile = req.body.excelFile;
-//   // let sheetName = req.body.excelFile;
-//
-//   const workbook = xlsx.readFile("sample.xlsx");
-//   const sheet = workbook.Sheets("Sheet1");
-//
-//   const range = xlsx.utils.decode_range(worksheet["!ref"]);
-//
-//   try {
-//
-//     for (let rowNumber = range.s.r;rowNumber <= range.e.r; rowNumber++){
-//       const email = sheet[xlsx.utils.encode_cell({r: rowNumber, c: 3})].v; //grabbing the email cell
-//
-//       if (email === "yushiko444@yahoo.com"){
-//         sheet[xlsx.utils.encode_cell({r: rowNumber, c: 3})].v + "jello";
-//       }
-//
-//       const newFile = xlsx.utils.book_new();
-//       xlsx.utils.book_append_sheet(newFile, sheet, "sheet1");
-//       xlsx.writeFile(newFile, "editedFile.xlsx");
-//     }
-//
-//     }catch(err){
-//       console.log(err);
-//     }
-// });
-
-
 const workbook = xlsx.readFile("../sample.xlsx");
 const sheet = workbook.Sheets["Sheet1"];
 
@@ -40,12 +11,22 @@ const range = xlsx.utils.decode_range(sheet["!ref"]);
 
 try {
 
-  for (let rowNumber = range.s.r; rowNumber <= range.e.r; rowNumber++){
-    let email = sheet[xlsx.utils.encode_cell({r: rowNumber, c: 2})].v;
+  for (let rowNumber = range.s.r + 1; rowNumber <= range.e.r; rowNumber++){
 
-    if (email === "yushiko333@yahoo.com"){
-            sheet[xlsx.utils.encode_cell({r: rowNumber, c: 2})].v  = sheet[xlsx.utils.encode_cell({r: rowNumber, c: 2})].v + "jello";
+    if (sheet[xlsx.utils.encode_cell({r: rowNumber, c: 2})] !== undefined){
+      let email = sheet[xlsx.utils.encode_cell({r: rowNumber, c: 2})].v;
+          email = email.replace(/\s/g, '').toLowerCase().split("@");
+
+      const commonEmailsArray = ["gmail.com", "yahoo.com", "i.softbank.jp", "icloud.com"]
+          for(let commonEmail = 0; commonEmail <= commonEmailsArray.length; commonEmail++){
+
+            if ((email[1] + commonEmailsArray[commonEmail]).split('').sort().join('').replace(/(.)\1+/g, "").length <= 1){
+              sheet[xlsx.utils.encode_cell({r: rowNumber, c: 2})].v = email[0] + "@" + commonEmailsArray[commonEmail];
+
+                  }
           }
+
+    }
 
     const newFile = xlsx.utils.book_new();
     xlsx.utils.book_append_sheet(newFile, sheet, "sheet1");
